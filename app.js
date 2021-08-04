7873f948f5ea9a6e6c6c47be33fb0ba174bf02f3
raywang999 <root@STU-18147702157.localdomain>
2021-08-03 20:59:27 -0400

    Setup project, import packages and test schema

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use('/graphql', graphqlHTTP({
+	schema: buildSchema(`
+		type RootQuery {
+			events: [String!]!
+		}
+
+		type RootMutation {
+			createEvent(name: String): String
+		}
+
+		schema {
+			query: RootQuery
+			mutation: RootMutation
+		}
+	`),
+	rootValue: {
+		events: () => {
+			return ['Romantic Cooking', 'Sailing', 'all-Night Coding']
+		},
+		createEvent: (args) => {
+			const eventName = args.name;
+			return eventName;
+		}
+	},
+	graphiql: true
+}));
+
+app.listen(3000);
