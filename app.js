76a9cb68d57b6614cb3a0db48d466f6c3c872bd3
raywang999 <root@STU-18147702157.localdomain>
2021-08-06 16:03:52 -0400

    Add frontend to login and signup
    In React Component AuthPage:
    - Created handlers for login and submiting login-information
    - Created form to obtain login information
    - Communicates information to our GraphQL API using POST requests
    
    Modified Backend Express app.js:
    - To allow CORS by enabling specified headers, request types and origin requests

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ const graphQlResolvers = require('./graphql/resolvers/index');
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+	if (req.method === 'OPTIONS') {
+		return res.sendStatus(200);
+	}
+	next();
+});
 app.use(isAuth);
 
 app.use('/graphql', graphqlHTTP({
@@ -21,6 +29,6 @@ app.use('/graphql', graphqlHTTP({
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD
 	}@cluster0.xvemn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-).then(() => {app.listen(3000);}).catch(err => {
+).then(() => {app.listen(8000);}).catch(err => {
 	console.log(err);
 });
