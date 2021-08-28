acd1792c080e12e3d19e2af69c4c07635ab3cad6
raywang999 <root@STU-18147702157.localdomain>
2021-08-27 22:42:46 -0400

    Update server and Schema to work with Apollo

--- app.js
+++ app.js
@@ -1,35 +1,68 @@
+const { fileURLToPath } = require('url');
+const makeDir = require('make-dir');
 const express = require('express');
-const bodyParser = require('body-parser');
-const { graphqlHTTP } = require('express-graphql');
+const http = require('http');
+const { ApolloServer } = require('apollo-server-express');
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core');
+const { graphqlUploadExpress } = require('graphql-upload');
 const mongoose = require('mongoose');
-const isAuth = require('./middleware/is-auth');
 
-const graphQlSchema = require('./graphql/schema/index');
+const UPLOAD_DIRECTORY_URL = require('./config/UPLOAD_DIRECTORY_URL.js');
+const typeDefs = require('./graphql/schema/index');
 const graphQlResolvers = require('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+//const schema = require('./schema/index.mjs');
 
-const app = express(); //middleware-framwork
+/**
+ * Starts the API server.
+ */
+// Ensure the upload directory exists.
+async function startServer() {
+	await makeDir(UPLOAD_DIRECTORY_URL);
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods','POST,GET,OPTIONS');
-	res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
-	res.setHeader('Cross-Origin-Opener-Policy','same-origin');
-	if (req.method === 'OPTIONS') {
-		return res.sendStatus(200);
-	}
-	next();
-});
-app.use(isAuth);
+	const app = new express();
+	app.use(graphqlUploadExpress({
+		maxFileSize: 10000000, // 10 MB
+		maxFiles: 20,
+	}));
+	app.use((req, res, next) => {
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS');
+		res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+		//res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+		if (req.method === 'OPTIONS') {
+			return res.sendStatus(200);
+		}
+		next();
+	});
+	app.use(isAuth);
+	const httpServer = new http.createServer(app);
 
-app.use('/graphql', graphqlHTTP({
-	schema: graphQlSchema,
-	rootValue: graphQlResolvers,
-	graphiql: true
-}));
+	/*app.use('/graphql', graphqlHTTP({
+		schema: graphQlSchema,
+		rootValue: graphQlResolvers,
+		graphiql: true
+	}));*/
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD
-	}@cluster0.xvemn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-).then(() => {app.listen(8000);}).catch(err => {
-	console.log(err);
-});
+	const apolloServer = new ApolloServer({
+		typeDefs: typeDefs,
+		plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+	});
+
+	await apolloServer.start();
+	apolloServer.applyMiddleware({
+		app,
+		path: '/'
+	});
+
+	await new Promise(resolve => httpServer.listen({ port: process.env.PORT }, resolve));
+	console.log(`🚀 Server ready at http://localhost:${process.env.PORT}/${apolloServer.graphqlPath}`);
+
+	mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD
+		}@cluster0.xvemn.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
+	).then(() => { app.listen(8000); }).catch(err => {
+		console.log(err);
+	});
+}
+
+startServer();
