2141712cbbfa8228c3d117e2d874d6bda5d906af
raywang999 <root@STU-18147702157.localdomain>
2021-08-27 21:07:31 -0400

    Update App.js to use Hooks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,53 @@
 import './App.scss';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
 
 import AuthPage from './pages/Auth';
 import ReferralsPage from './pages/Referrals';
 import PatientsPage from './pages/Patients';
 import MainNavigation from './components/navigation/MainNavigation';
-import React from 'react';
 import AuthContext from './context/auth-context';
 import TestsPage from './pages/Tests';
 
-class App extends React.Component {
-  state = {
-    token: null,
-    userId: null,
-    email: null,
+export default () => {
+  const [token, setToken] = useState(null);
+  const [userId, setUserId] = useState(null);
+  const [email, setEmail] = useState(null);
+
+  const login = (token, userId, tokenExpiration, email) => {
+    setToken(token); setUserId(userId); setEmail(email);
   }
 
-  login = (token, userId, tokenExpiration, email) => {
-    this.setState({ token: token, userId: userId, email: email});
+  const logout = () => {
+    setToken(null); setUserId(null); setEmail(null);
   }
 
-  logout = () => {
-    this.setState({ token: null, userId: null, email: null });
-  }
-
-  render() {
-    return (
-      <BrowserRouter>
-        <React.Fragment>
-          <AuthContext.Provider
-            value={{
-              token: this.state.token,
-              userId: this.state.userId,
-              email: this.state.email,
-              login: this.login,
-              logout: this.logout
-            }}
-          >
-            <MainNavigation />
-            <main className="main-content">
-              <Switch>
-                {this.state.token && <Redirect from="/" to="/patients" exact />}
-                {this.state.token && <Redirect from="/auth" to="/patients" exact />}
-                {/*<Route path="/test" component={TestsPage} />*/}
-                {!this.state.token && (<Route path="/auth" component={AuthPage} />)}
-                {this.state.token && (<Route path="/patients" component={PatientsPage} />)}
-                {this.state.token && (<Route path="/referrals" component={ReferralsPage} />)}
-                {!this.state.token && <Redirect to="/auth" exact />}
-              </Switch>
-            </main>
-          </AuthContext.Provider>
-        </React.Fragment>
-      </BrowserRouter>
-    );
-  }
+  return (
+    <BrowserRouter>
+      <React.Fragment>
+        <AuthContext.Provider
+          value={{
+            token: token,
+            userId: userId,
+            email: email,
+            login: login,
+            logout: logout,
+          }}
+        >
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              {token && <Redirect from="/" to="/patients" exact />}
+              {token && <Redirect from="/auth" to="/patients" exact />}
+              {/*<Route path="/test" component={TestsPage} />*/}
+              {!token && (<Route path="/auth" component={AuthPage} />)}
+              {token && (<Route path="/patients" component={PatientsPage} />)}
+              {token && (<Route path="/referrals" component={ReferralsPage} />)}
+              {!token && <Redirect to="/auth" exact />}
+            </Switch>
+          </main>
+        </AuthContext.Provider>
+      </React.Fragment>
+    </BrowserRouter>
+  );
 }
-
-export default App;
